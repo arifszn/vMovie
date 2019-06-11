31078003dba2db1279efef412fe0f41c60f35b12
arifszn <swazan.arif@gmail.com>
2019-06-12 03:21:49 +0600

    Initialization

---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light text-lowercase">v</span>
+                <span>Dairy</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+
+
+            <v-btn flat round color="grey" class="hidden-sm-and-down">
+                <span>Now Playing</span>
+            </v-btn>
+            <v-btn flat round color="grey" class="hidden-sm-and-down">
+                <span>Upcoming</span>
+            </v-btn>
+            <v-btn flat round color="grey" class="hidden-sm-and-down">
+                <span>Popular</span>
+            </v-btn>
+            <v-btn flat round color="grey" class="hidden-sm-and-down">
+                <span>Top Rated</span>
+            </v-btn>
+            <v-spacer></v-spacer>
+             
+
+            <v-btn flat round color="grey">
+                <span>Sign In</span>
+            </v-btn>
+
+            <v-btn flat outline round small class="success" color="grey">
+                <span>Sign Up</span>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="primary">
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/avatar-1.png" alt="">
+                    </v-avatar>
+                </v-flex>
+                    <p class="white--text subheading mt-1">John Doe</p>
+                <v-flex class="mt-4 mb-3">
+                </v-flex>
+            </v-layout>
+            <v-list>
+                <v-list-tile v-for="link in links" :key="link.text" router :to="link.route" >
+                    <v-list-tile-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            links: [
+                {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                {icon: 'folder', text: 'My Projects', route: '/projects'},
+                {icon: 'person', text: 'Team', route: '/team'}
+            ],
+        }
+    },
+    components: {
+        
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="home">
+    <h1 class="subheading grey--text">Home</h1>
+
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs6 sm6 md3 lg2 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive>
+              <v-img src="/godzilla.jpg" ></v-img>
+            </v-responsive>
+            <v-card-text>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        team: [
+        { name: 'John Doe', role: 'Web developer', avatar: '/avatar-1.png' },
+        { name: 'Miles Morales', role: 'Graphics designer', avatar: '/avatar-2.png' },
+        { name: 'Clark Kent', role: 'Software Engineer', avatar: '/avatar-3.png' },
+        { name: 'Peter Parker', role: 'Web developer', avatar: '/avatar-4.png' },
+        { name: 'Peter Parker', role: 'Web developer', avatar: '/avatar-4.png' },
+
+      ]
+      }
+    }
   }
-}
 </script>
+
+<style>
+
+</style>
+
